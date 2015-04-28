929c65562d5a3886e3f070390a54610c3d19c373
sebastiendeszcz <sebastien.deszcz@gmail.com>
2015-04-28 19:57:51 +0200

    Ajout des pages Fournisseur/Stock + CSS

---
+++ src/main/java/com/easygeek/controller/StockController.java
@@ -0,0 +1,29 @@
+package com.easygeek.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.easygeek.dao.StockDao;
+import com.easygeek.entite.Stock;
+
+@Controller
+public class StockController {
+	
+	public ModelAndView mav = new ModelAndView();
+
+	@Autowired
+	StockDao stockDao;
+	
+	@RequestMapping(value = "/stocks", method = RequestMethod.GET)
+	public ModelAndView getStocks() {
+		mav.setViewName("stocks");
+
+		mav.addObject("command", new Stock());
+		mav.addObject("stockList", stockDao.getAll());
+
+		return mav;
+	}
+}
